f06db3af01daec1c87550e1c14b1a99f895ccbd8
Nataliya Trondina <trondina.nataliya@yandex.ru>
2025-05-14 10:29:39 +0400

    выполнено задание девятого урока

--- js/script.js
+++ js/script.js
@@ -1,126 +1,164 @@
 "use strict";
 
-const appData = {
-  title: "",
-  screens: [],
-  screenPrice: 0,
-  adaptive: true,
-  rollback: 10,
-  allServicePrices: 0,
-  fullPrice: 0,
-  servicePercentPrice: 0,
-  services: {},
-  start: () => {
-    appData.asking();
-    appData.addPrices();
-    appData.getFullPrice();
-    appData.getServicePercentPrice();
-    appData.getTitle();
-    appData.logger();
-  },
+// 1. Получить заголовок "Калькулятор верстки" через метод getElementsByTagName. (тэг h1, получить именно элемент, а не коллекцию)
+const title = document.getElementsByTagName("h1");
 
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
+//2. Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName. (класс handler_btn)
+const buttons = document.getElementsByClassName("handler_btn");
 
-  asking: function () {
-    do {
-      appData.title = prompt(
-        "Как называется ваш проект?",
-        "Калькулятор верстки"
-      );
-    } while (appData.isNumber(appData.title));
+//3. Получить кнопку "+" под выпадающим списком через метод querySelector. (класс screen-btn)
+const plusButton = document.querySelector(".screen-btn");
 
-    for (let i = 0; i < 2; i++) {
-      let price = 0;
-      let name = "";
-      do {
-        name = prompt("Какие типы экранов нужно разработать?");
-      } while (appData.isNumber(name));
+//4. Получить все элементы с классом other-items в две разные переменные. В первую элементы у которых так же присутствует класс percent, во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
+const percentItems = document.querySelectorAll(".other-items .percent");
+const numberItems = document.querySelectorAll(".other-items .number");
 
-      do {
-        price = +prompt("Сколько будет стоить данная работа?");
-      } while (!appData.isNumber(price));
+//5. Получить input type=range через его родителя с классом rollback одним запросом через метод querySelector.
+const rollbackInput = document.querySelector(".rollback input[type='range']");
 
-      appData.screens.push({ id: i, name: name, price: price });
-    }
+//6. Получить span с классом range-value через его родителя с классом rollback одним запросом через метод querySelector.
+const rollbackSpan = document.querySelector(".rollback .range-value");
 
-    for (let i = 0; i < 2; i++) {
-      let name = "";
-      let price = 0;
+//7. Получить все инпуты с классом total-input справа через метод getElementsByClassName. (класс total-input, получить именно элементы, а не коллекции)
+const inputs = document.getElementsByClassName("total-input");
 
-      do {
-        name = prompt("Какой дополнительный тип услуги нужен?");
-      } while (appData.isNumber(name));
+[...inputs].forEach(function (item) {
+  console.log(item);
+});
 
-      do {
-        price = prompt("Сколько это будет стоить?");
-      } while (!appData.isNumber(price));
+//8. Получить все блоки с классом screen в изменяемую переменную ( let ) через метод querySelectorAll (далее мы будем переопределять ее значение)
+let screen = document.querySelectorAll(".screen");
 
-      // appData.services[name] = +price;
+console.log(title[0]);
+console.log(buttons);
+console.log(plusButton);
+console.log(percentItems);
+console.log(numberItems);
+console.log(rollbackInput);
+console.log(rollbackSpan);
+console.log(screen);
 
-      //Создадим уникальный ключ для услуги
-      let uniqueKey = name;
-      let count = 1;
+// const appData = {
+//   title: "",
+//   screens: [],
+//   screenPrice: 0,
+//   adaptive: true,
+//   rollback: 10,
+//   allServicePrices: 0,
+//   fullPrice: 0,
+//   servicePercentPrice: 0,
+//   services: {},
+//   start: () => {
+//     appData.asking();
+//     appData.addPrices();
+//     appData.getFullPrice();
+//     appData.getServicePercentPrice();
+//     appData.getTitle();
+//     appData.logger();
+//   },
 
-      //Проверяем, существует ли уже такая услуга
-      while (appData.services[uniqueKey]) {
-        count++;
-        uniqueKey = `${name}_${count}`; // добавляем номер к имени
-      }
+//   isNumber: function (num) {
+//     return !isNaN(parseFloat(num)) && isFinite(num);
+//   },
 
-      appData.services[uniqueKey] = +price; // сохраняем услугу с уникальным ключом
-    }
+//   asking: function () {
+//     do {
+//       appData.title = prompt(
+//         "Как называется ваш проект?",
+//         "Калькулятор верстки"
+//       );
+//     } while (appData.isNumber(appData.title));
 
-    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-  },
+//     for (let i = 0; i < 2; i++) {
+//       let price = 0;
+//       let name = "";
+//       do {
+//         name = prompt("Какие типы экранов нужно разработать?");
+//       } while (appData.isNumber(name));
 
-  addPrices: function () {
-    for (let screen of appData.screens) {
-      // appData.screenPrice += +screen.price;
-      appData.screenPrice = appData.screens.reduce(
-        (acc, screen) => acc + screen.price,
-        0
-      );
-    }
+//       do {
+//         price = +prompt("Сколько будет стоить данная работа?");
+//       } while (!appData.isNumber(price));
 
-    for (let key in appData.services) {
-      appData.allServicePrices += appData.services[key];
-    }
-  },
+//       appData.screens.push({ id: i, name: name, price: price });
+//     }
 
-  getFullPrice: function () {
-    appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
-  },
+//     for (let i = 0; i < 2; i++) {
+//       let name = "";
+//       let price = 0;
 
-  getServicePercentPrice: function () {
-    appData.servicePercentPrice =
-      appData.fullPrice - (appData.fullPrice * appData.rollback) / 100;
-  },
+//       do {
+//         name = prompt("Какой дополнительный тип услуги нужен?");
+//       } while (appData.isNumber(name));
 
-  getTitle: function () {
-    appData.title =
-      appData.title.trim()[0].toUpperCase() +
-      appData.title.trim().slice(1).toLowerCase();
-  },
+//       do {
+//         price = prompt("Сколько это будет стоить?");
+//       } while (!appData.isNumber(price));
 
-  getRollbackMessage: (price) => {
-    if (price >= 30000) {
-      return "Даем скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-      return "Даем скидку в 5%";
-    } else if (price >= 0 && price < 15000) {
-      return "Скидка не предусмотрена";
-    } else {
-      return "Что то пошло не так";
-    }
-  },
+//       // appData.services[name] = +price;
 
-  logger: () => {
-    console.log(appData.fullPrice);
-    console.log(appData.servicePercentPrice);
-    console.log(appData.screens);
-  },
-};
+//       //Создадим уникальный ключ для услуги
+//       let uniqueKey = name;
+//       let count = 1;
 
-appData.start();
+//       //Проверяем, существует ли уже такая услуга
+//       while (appData.services[uniqueKey]) {
+//         count++;
+//         uniqueKey = `${name}_${count}`; // добавляем номер к имени
+//       }
+
+//       appData.services[uniqueKey] = +price; // сохраняем услугу с уникальным ключом
+//     }
+
+//     appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+//   },
+
+//   addPrices: function () {
+//     for (let screen of appData.screens) {
+//       // appData.screenPrice += +screen.price;
+//       appData.screenPrice = appData.screens.reduce(
+//         (acc, screen) => acc + screen.price,
+//         0
+//       );
+//     }
+
+//     for (let key in appData.services) {
+//       appData.allServicePrices += appData.services[key];
+//     }
+//   },
+
+//   getFullPrice: function () {
+//     appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
+//   },
+
+//   getServicePercentPrice: function () {
+//     appData.servicePercentPrice =
+//       appData.fullPrice - (appData.fullPrice * appData.rollback) / 100;
+//   },
+
+//   getTitle: function () {
+//     appData.title =
+//       appData.title.trim()[0].toUpperCase() +
+//       appData.title.trim().slice(1).toLowerCase();
+//   },
+
+//   getRollbackMessage: (price) => {
+//     if (price >= 30000) {
+//       return "Даем скидку в 10%";
+//     } else if (price >= 15000 && price < 30000) {
+//       return "Даем скидку в 5%";
+//     } else if (price >= 0 && price < 15000) {
+//       return "Скидка не предусмотрена";
+//     } else {
+//       return "Что то пошло не так";
+//     }
+//   },
+
+//   logger: () => {
+//     console.log(appData.fullPrice);
+//     console.log(appData.servicePercentPrice);
+//     console.log(appData.screens);
+//   },
+// };
+
+// appData.start();
